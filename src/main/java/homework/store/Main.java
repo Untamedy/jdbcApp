914f61279736412bf5f27c1a8919ea7f0a7f9665
Untamedy <Untamedy@bigmir.net>
2019-09-15 00:06:49 +0300

    no message

--- src/main/java/homework/store/Main.java
+++ src/main/java/homework/store/Main.java
@@ -1,15 +1,5 @@
 package homework.store;
 
-import homework.commonInit.ConnectionService;
-import homework.commonInit.InputData;
-import homework.commonInit.PropertyReader;
-import homework.flats.entities.Flat;
-import homework.flats.services.FilterService;
-import homework.store.entities.Goods;
-import homework.store.entities.Order;
-import homework.store.services.StoreService;
-import java.sql.SQLException;
-import java.util.List;
 
 /**
  *
@@ -17,7 +7,7 @@ import java.util.List;
  */
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
        
     }
